05261bed307d985cd7e6691ce44e90b642b7fb43
emmanuel <emmanuel.roberson3@gmail.com>
2020-12-22 20:12:25 -0600

    Imported RPG Monsters and Polygon asset pack. Added mouse controlled looking

---
+++ Start Small/Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            characterController.Move(transform.right * moveSpeed);
+
+        if (Input.GetAxis("Horizontal") > 0)
+            characterController.Move(-transform.right * moveSpeed);
+    }
+}
